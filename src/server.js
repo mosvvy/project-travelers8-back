75a800c4d724ff3376e74df4835b239ff43d40a7
Valeriia Moskalenko <50095851+mosvvy@users.noreply.github.com>
2026-02-19 11:51:06 +0200

    Repo prep (#2)
    * add project files. add server.js. add routes files
    
    * update package.json
    
    * add files. add base middlewares
    
    * add celebrate dependency

---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+
+import storiesRoutes from './routes/storiesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import usersRoutes from './routes/usersRoutes.js';
+import { errors } from 'celebrate';
+// import cookieParser from 'cookie-parser';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(logger);
+app.use(express.json());
+app.use(cors());
+// app.use(cookieParser());
+
+app.use(authRoutes);
+app.use(usersRoutes);
+app.use(storiesRoutes);
+
+app.use(notFoundHandler);
+
+app.use(errors());
+
+app.use(errorHandler);
+
+await connectMongoDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
