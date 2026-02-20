2b553b084984bbfbca73edc204d64b6e2e340312
Roman <neirity@gmail.com>
2026-02-20 16:57:31 +0200

    add mongo models (#17)

---
+++ src/models/category.js
@@ -0,0 +1,5 @@
+import { Schema, model } from 'mongoose';
+const categorySchema = new Schema({
+  name: { type: String, required: true, trim: true },
+});
+export const Category = model('Category', categorySchema);
