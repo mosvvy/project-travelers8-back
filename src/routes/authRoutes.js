35a696f9b6adaff7e72d60aa9196e7240d438997
Ivan Shakhov <ivanshahov04@gmail.com>
2026-02-21 17:28:46 +0200

    fixes_2

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,9 +4,5 @@ import { registerController } from '../controllers/authController.js';
 import { registerSchema } from '../validation/authValidation.js';
 const router = Router();
 
-router.post(
-  '/api/auth/register',
-  celebrate(registerSchema),
-  registerController,
-);
+router.post('/auth/register', celebrate(registerSchema), registerController);
 export default router;
