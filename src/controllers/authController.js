68f0c68652e14648ecbfd5a77fe4bfc004eab1d7
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-02-21 11:08:36 +0100

    feat(auth): implement public login endpoint

---
+++ src/controllers/authController.js
@@ -0,0 +1,32 @@
+import bcrypt from 'bcrypt';
+import createHttpError from 'http-errors';
+import User from '../models/User.js';
+import Session from '../models/session.js';
+import { createSession, setSessionCookies } from '../services/auth.js';
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Перевіряємо чи поля передані
+  if (!email || !password) {
+    throw createHttpError(400, 'Email and password required');
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+
+  if (!isValidPassword) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  await Session.deleteOne({ userId: user._id });
+
+  const newSession = await createSession(user._id);
+  setSessionCookies(res, newSession);
+  res.status(200).json(user);
+};
