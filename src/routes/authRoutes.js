5c832732829ac03fda331694aceb5a5557cfe228
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-02-21 11:09:07 +0100

    feat(auth): register POST /login route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,4 +1,10 @@
 import { Router } from 'express';
+import { celebrate } from 'celebrate';
+import { loginUser } from '../controllers/authController.js';
+import { loginUserSchema } from '../validations/authValidation.js';
+
 const router = Router();
+
+router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 
 export default router;
