31a1581c48f4aacff2459d416159763e5e32cf6b
Dmytro Bykanov <dmitbykanov@gmail.com>
2026-02-21 17:14:02 +0200

    add storiesValidation file (#23)

---
+++ src/validations/storiesValidation.js
@@ -0,0 +1,17 @@
+import { Joi, Segments } from 'celebrate';
+import { isValidObjectId } from 'mongoose';
+
+const objectIdValidator = (value, helpers) => {
+  if (!isValidObjectId(value)) {
+    return helpers.message('Invalid id format');
+  }
+  return value;
+};
+
+export const getAllStoriesSchema = {
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number().integer().min(1).default(1),
+    limit: Joi.number().integer().min(1).max(50).default(9),
+    category: Joi.string().custom(objectIdValidator),
+  }),
+};
