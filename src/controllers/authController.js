38a0946339ad9e8eee3d3d8cb99b76ea78623fb0
ivanshakhov128 <107489189+ivanshakhov128@users.noreply.github.com>
2026-02-23 10:47:35 +0200

    register endpoint without model (#19)
    * register endpoint without model
    
    * fixes
    
    * fixes_2
    
    * removed from services to controller
    
    * merge imports

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,29 @@ import { User } from '../models/user.js';
 import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 
+export const registerController = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existing = await User.findOne({ email });
+    if (existing) throw createHttpError(409, 'Email already in use');
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: passwordHash,
+    });
+
+    res.status(201).json({
+      user: { id: user._id, name: user.name, email: user.email },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
 export const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,19 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { loginUser, logoutUser } from '../controllers/authController.js';
-import { loginUserSchema } from '../validations/authValidation.js';
+import {
+  registerController,
+  loginUser,
+  logoutUser,
+} from '../controllers/authController.js';
+import {
+  loginUserSchema,
+  registerSchema,
+} from '../validations/authValidation.js';
 
 const router = Router();
 
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+router.post('/auth/register', celebrate(registerSchema), registerController);
 router.post('/auth/logout', logoutUser);
 
 export default router;
