523a7a1b9855619366d931550238a44bd1b0a9ae
ANASTASIIA <nastamanzuk62@gmail.com>
2026-02-23 17:43:37 +0100

    chore: clean up branch and sync with main

--- src/routes/storiesRoutes.js
+++ src/routes/storiesRoutes.js
@@ -1,16 +1,5 @@
 import { Router } from 'express';
-import { celebrate } from 'celebrate';
 
-import { getFavouriteStoriesSchema } from '../validations/storyValidation.js';
-import { getFavouriteStories } from '../controllers/storiesController.js';
-import { authenticate } from '../middleware/authenticate.js';
 const router = Router();
-
-router.get(
-  '/stories/favourite',
-  authenticate,
-  celebrate(getFavouriteStoriesSchema),
-  getFavouriteStories,
-);
 
 export default router;
--- src/validations/storyValidation.js
+++
@@ -1,8 +0,0 @@
-import { Joi, Segments } from 'celebrate';
-
-export const getFavouriteStoriesSchema = {
-  [Segments.QUERY]: Joi.object({
-    page: Joi.number().integer().min(1).default(1),
-    perPage: Joi.number().integer().min(1).max(100).default(10),
-  }),
-};
