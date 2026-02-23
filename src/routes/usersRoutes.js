1e0067349868d46eb9b02ab787c8b0bdcec465ac
Mila <miladiachenko2404@gmail.com>
2026-02-24 00:28:08 +0200

    feat: add current user endpoint

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { authenticate } from '../middleware/authenticate.js';
+import {getCurrentUser} from '../controllers/userController.js';
 const router = Router();
-
+router.get('/users/me', authenticate, getCurrentUser);
 export default router;
