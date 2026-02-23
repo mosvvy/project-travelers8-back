b0383facbe7a9167899f655c647494dc392d4680
Marina <marinamaksimovskaa3@gmail.com>
2026-02-23 21:03:43 +0200

    add GET /users endpoint with pagination

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
-import { authenticate } from '../middleware/authenticate.js';
+import { getUsers } from '../controllers/usersController.js';
+
 const router = Router();
+
+router.get('/users', getUsers);
 
 export default router;
