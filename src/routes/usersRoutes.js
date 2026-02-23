1085ba6baf8cfc9d2d14106575286b88840eaa62
Marina3003 <marinamaksimovskaa3@gmail.com>
2026-02-24 00:00:38 +0300

    Update usersRoutes.js

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { authenticate } from '../middleware/authenticate.js';
 import { getUsers } from '../controllers/usersController.js';
 
 const router = Router();
 
 router.get('/users', getUsers);
 
-export default router; 
+export default router;
