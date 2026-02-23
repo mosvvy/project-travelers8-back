bcf627de816c741cb81f33b622807142efb9689a
Dmytro Bykanov <dmitbykanov@gmail.com>
2026-02-23 22:06:49 +0200

    add getALLStories (#24)
    * add getALLStories
    
    * update storiesController
    
    * upd
    
    * update getAll
    
    * update

--- src/routes/storiesRoutes.js
+++ src/routes/storiesRoutes.js
@@ -1,5 +1,9 @@
 import { Router } from 'express';
-
+import { celebrate } from 'celebrate';
+import { getAllStories } from '../controllers/storiesController.js';
+import { getAllStoriesSchema } from '../validations/storiesValidation.js';
 const router = Router();
+
+router.get('/stories', celebrate(getAllStoriesSchema), getAllStories);
 
 export default router;
