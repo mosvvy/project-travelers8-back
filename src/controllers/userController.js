272e48f39ae53d00c9657faad4945c32773c6e43
Iryna Yeremizina <yeremizina1@gmail.com>
2026-02-24 18:18:44 +0200

    change..

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,48 @@
+// import createHttpError from 'http-errors';
+// import mongoose from 'mongoose';
+// import { User } from '../models/user.js';
+// import { Story } from '../models/story.js';
+
+// export const saveStory = async (req, res, next) => {
+//   try {
+//     const { storyId } = req.params;
+//     const userId = req.user._id;
+
+//     if (!mongoose.Types.ObjectId.isValid(storyId)) {
+//       return next(createHttpError(400, 'Invalid story ID format'));
+//     }
+
+//     const story = await Story.findById(storyId);
+//     if (!story) {
+//       return next(createHttpError(404, 'Story not found'));
+//     }
+
+//     const user = await User.findById(userId);
+//     if (!user) {
+//       return next(createHttpError(404, 'User not found'));
+//     }
+
+//     const isSaved = user.savedStories.includes(storyId);
+//     const operation = isSaved ? '$pull' : '$addToSet';
+
+//     const updatedUser = await User.findByIdAndUpdate(
+//       userId,
+//       { [operation]: { savedStories: storyId } },
+//       { new: true },
+//     );
+
+//     res.status(200).json({
+//       message: isSaved ? 'Story removed from saved' : 'Story added to saved',
+//       data: {
+//         savedStories: updatedUser.savedStories,
+//       },
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
 import createHttpError from 'http-errors';
-import mongoose from 'mongoose';
 import { User } from '../models/user.js';
 import { Story } from '../models/story.js';
 
@@ -7,10 +50,6 @@ export const saveStory = async (req, res, next) => {
   try {
     const { storyId } = req.params;
     const userId = req.user._id;
-
-    if (!mongoose.Types.ObjectId.isValid(storyId)) {
-      return next(createHttpError(400, 'Invalid story ID format'));
-    }
 
     const story = await Story.findById(storyId);
     if (!story) {
