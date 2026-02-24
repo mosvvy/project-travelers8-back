4b28d0e18606cc0722d12e4c3405cd532c22135d
ValeriiaM <moskalenkovvy@gmail.com>
2026-02-24 18:34:47 +0200

    add image processing. add multer.

--- src/controllers/storiesController.js
+++ src/controllers/storiesController.js
@@ -1,18 +1,29 @@
 import { Story } from '../models/story.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+
+const getCurrentDate = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
+  return `${day < 10 ? '0' + day : day}-${month < 10 ? '0' + month : month}-${year}`;
+};
 
 export const createStory = async (req, res) => {
-  console.log('New story:', req.body);
+  if (!req.file) {
+    next(createHttpError(400, 'No file'));
+    return;
+  }
 
-  const newStory = {
+  const result = await saveFileToCloudinary(req.file.buffer);
+
+  const newStory = await Story.create({
     ...req.body,
     ownerId: req.user._id,
-    // category: req.body.category,
-  };
+    category: req.body.category,
+    date: getCurrentDate(),
+    img: result.secure_url,
+  });
 
-  // const newStory = await Story.create({
-  //   ...req.body,
-  //   ownerId: req.user._id,
-  //   // category: req.body.category,
-  // });
   res.status(201).json(newStory);
 };
--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -3,7 +3,7 @@ import multer from 'multer';
 export const upload = multer({
   storage: multer.memoryStorage(),
   limits: {
-    fileSize: 2 * 1024 * 1024,
+    fileSize: 500 * 1024,
   },
   fileFilter: (req, file, cb) => {
     if (!file.mimetype || !file.mimetype.startsWith('image/')) {
