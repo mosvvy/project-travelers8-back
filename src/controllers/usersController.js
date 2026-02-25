20e8dbc2a2b7711670b9aab913a3ab5ec163a944
Iryna Yeremizina <yeremizina1@gmail.com>
2026-02-25 19:00:27 +0200

    consolidate user controllers and implement toggle save story

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,29 +1,6 @@
-import { User } from '../models/user.js';
-
 import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import { Story } from '../models/story.js';
-
-export const getUsers = async (req, res, next) => {
-  try {
-    const page = parseInt(req.query.page) || 1;
-    const perPage = parseInt(req.query.perPage) || 10;
-    const skip = (page - 1) * perPage;
-
-    const totalUsers = await User.countDocuments();
-    const users = await User.find().skip(skip).limit(perPage);
-
-    res.status(200).json({
-      page,
-      perPage,
-      totalUsers,
-      totalPages: Math.ceil(totalUsers / perPage),
-      users,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
 
 export const saveStory = async (req, res, next) => {
   try {
@@ -57,5 +34,26 @@ export const saveStory = async (req, res, next) => {
     });
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUsers = async (req, res, next) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const perPage = parseInt(req.query.perPage) || 10;
+    const skip = (page - 1) * perPage;
+
+    const totalUsers = await User.countDocuments();
+    const users = await User.find().skip(skip).limit(perPage);
+
+    res.status(200).json({
+      page,
+      perPage,
+      totalUsers,
+      totalPages: Math.ceil(totalUsers / perPage),
+      users,
+    });
+  } catch (err) {
+    next(err);
   }
 };
