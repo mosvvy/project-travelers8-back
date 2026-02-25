8861c32c42788db3f7c2c748296b10f7d64629a2
Iryna Yeremizina <yeremizina1@gmail.com>
2026-02-25 18:19:35 +0200

    add files in usersController.js

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,5 +1,9 @@
 import { User } from '../models/user.js';
 
+import createHttpError from 'http-errors';
+import { User } from '../models/user.js';
+import { Story } from '../models/story.js';
+
 export const getUsers = async (req, res, next) => {
   try {
     const page = parseInt(req.query.page) || 1;
@@ -7,9 +11,7 @@ export const getUsers = async (req, res, next) => {
     const skip = (page - 1) * perPage;
 
     const totalUsers = await User.countDocuments();
-    const users = await User.find()
-      .skip(skip)
-      .limit(perPage);
+    const users = await User.find().skip(skip).limit(perPage);
 
     res.status(200).json({
       page,
@@ -20,5 +22,40 @@ export const getUsers = async (req, res, next) => {
     });
   } catch (err) {
     next(err);
+  }
+};
+
+export const saveStory = async (req, res, next) => {
+  try {
+    const { storyId } = req.params;
+    const userId = req.user._id;
+
+    const story = await Story.findById(storyId);
+    if (!story) {
+      return next(createHttpError(404, 'Story not found'));
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return next(createHttpError(404, 'User not found'));
+    }
+
+    const isSaved = user.savedStories.includes(storyId);
+    const operation = isSaved ? '$pull' : '$addToSet';
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { [operation]: { savedStories: storyId } },
+      { new: true },
+    );
+
+    res.status(200).json({
+      message: isSaved ? 'Story removed from saved' : 'Story added to saved',
+      data: {
+        savedStories: updatedUser.savedStories,
+      },
+    });
+  } catch (error) {
+    next(error);
   }
 };
