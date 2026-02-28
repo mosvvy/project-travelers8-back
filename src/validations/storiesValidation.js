4d00f3080d0853f6d8e47eb9f00c1fb28861e9a7
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-02-28 17:29:45 +0100

    Use objectIdValidator to validate ID in getStory

--- src/validations/storiesValidation.js
+++ src/validations/storiesValidation.js
@@ -20,11 +20,7 @@ export const createStorySchema = {
 
 export const getStoryByIdSchema = {
   [Segments.PARAMS]: Joi.object({
-    id: Joi.string().hex().length(24).required().messages({
-      'string.hex': 'Invalid user id format',
-      'string.length': 'Invalid user id length',
-      'any.required': 'User id is required',
-    }),
+    id: Joi.string().custom(objectIdValidator).required(),
   }),
 };
 
